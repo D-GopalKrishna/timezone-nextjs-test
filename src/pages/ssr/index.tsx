fd19d419f34e2bd13ec12e566ee1ab9712579a1c
D. Gopal Krishna <nikhil0223@gmail.com>
2023-03-07 16:27:27 +0530

    new timezone updates for new york

--- src/pages/ssr/index.tsx
+++ src/pages/ssr/index.tsx
@@ -8,7 +8,7 @@ import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz'
 
 type Props = {
     data: any, 
-    data2: any
+    data2: any,
 }
 
 export default function Page(props: Props) {
@@ -16,9 +16,15 @@ export default function Page(props: Props) {
     const timeZone = props.data.timezone
     const zonedDate = utcToZonedTime(date, timeZone)
     
-    const date2 = new Date(props.data2.dateTime)    // this converts the date to the client's timezone... 
-    const timeZone2 = props.data2.timeZone
+    const date2 = new Date(props.data2.utc_datetime)    // this converts the date to the client's timezone... 
+    const timeZone2 = props.data2.timezone
     const zonedDate2 = utcToZonedTime(date2, timeZone2)      // client's timezone time is converted to the timezone given. 
+    
+    const NewYorkTimeInUTC = zonedTimeToUtc(new Date('2018-09-01 18:01:36.386'), 'America/New_York')
+    console.log("NewYorkTimeInUTC", NewYorkTimeInUTC)
+    const NewYorkTimeInIST = utcToZonedTime(NewYorkTimeInUTC, 'Asia/Kolkata')
+    console.log("NewYorkTimeInIST", NewYorkTimeInIST)
+
     
     console.log("zonedDate", zonedDate.toString(), "date", date, "timeZone", timeZone)
     console.log("zonedDate2", zonedDate2.toString(), "date2", date2, "timeZone2", timeZone2)
@@ -35,8 +41,14 @@ export default function Page(props: Props) {
             <p>zonedDate - {zonedDate.toString()}</p>
 
             <hr />
-            <p>dateTime - {props.data2?.dateTime.toString()}</p>
-            <p>timeZone - {props.data2?.timeZone.toString()}</p>
+
+            <p>dateTime2 - {props.data2?.datetime.toString()}</p>
+            <p>utc_datetime2 - {props.data2?.utc_datetime.toString()}</p>
+            <p>utc_datetime converted to client (which is also given by an attribute) timezone- {utcToZonedTime(props.data?.utc_datetime.toString(), timeZone2).toString()}</p>
+            <p>utc_datetime converted to client (which is also given by an attribute) timezone- {format(utcToZonedTime(props.data?.utc_datetime.toString(), timeZone2),  'yyyy-MM-dd HH:mm:ss')}</p>
+            <p>utc_datetime converted to client (which is also given by an attribute) timezone (in ISO string) - {utcToZonedTime(props.data?.utc_datetime, timeZone2).toISOString()}</p>
+            <p>utc_datetime converted to client (which is also given by an attribute) timezone (in UTC string) - {utcToZonedTime(props.data?.utc_datetime, timeZone2).toUTCString()}</p>
+            <p>timezone - {props.data2?.timezone.toString()}</p>
             <p>zonedDate2 - {zonedDate2.toString()}</p>
         </div>
     )
@@ -46,21 +58,8 @@ export default function Page(props: Props) {
 export async function getServerSideProps(context: any) {
     const res = await fetch(`http://worldtimeapi.org/api/timezone/Asia/Kolkata`)
     const data = await res.json()
-    const data2 = {
-        "year": 2023,
-        "month": 3,
-        "day": 6,
-        "hour": 13,
-        "minute": 9,
-        "seconds": 14,
-        "milliSeconds": 565,
-        "dateTime": "2023-03-06T13:09:14.5650545",
-        "date": "03/06/2023",
-        "time": "13:09",
-        "timeZone": "Europe/Amsterdam",
-        "dayOfWeek": "Monday",
-        "dstActive": false
-    }
+    const res2 = await fetch(`http://worldtimeapi.org/api/timezone/America/New_York`)
+    const data2 = await res2.json()
 
-    return { props: { data: data, data2: data2 } }
+    return { props: { data: data, data2: data2 }}
 }
